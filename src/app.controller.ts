bfc6b80350acd26bccc0e8d3e06da461b804a9ad
chidi-godwin <chidieberen1998@gmail.com>
2022-12-02 05:56:26 +0100

    document health check route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({ summary: 'Health Check' })
   @Get()
   getHello(): string {
     return this.appService.getHello();
